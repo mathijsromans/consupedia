f9fead6e748f1822f06415e42ca4f8f0ce79d9b8
Bart Römgens <bart.romgens@gmail.com>
2017-03-05 18:05:59 +0100

    create product model with name and price

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product
+from product.models import ProductName, Price
+
+
+class PriceInline(admin.TabularInline):
+    max_num = 1
+    model = Price
+
+
+class ProductNameInline(admin.TabularInline):
+    max_num = 1
+    model = ProductName
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        PriceInline,
+        ProductNameInline,
+    ]
+
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+
+    @property
+    def name(self):
+        names = ProductName.objects.filter(product=self).order_by('-datetime_created')
+        if names:
+            return names[0]
+        return None
+
+    @property
+    def price(self):
+        prices = Price.objects.filter(product=self).order_by('-datetime_created')
+        if prices:
+            return prices[0]
+        return None
+
+
+class Revision(models.Model):
+    user = models.ForeignKey(User)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-datetime_created']
+
+
+class ProductName(Revision):
+    name = models.CharField(max_length=300)
+    product = models.ForeignKey(Product)
+
+
+class Price(Revision):
+    cent = models.IntegerField(default=0)
+    product = models.ForeignKey(Product)
+
