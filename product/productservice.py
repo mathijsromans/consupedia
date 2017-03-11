e39c50784bc74341d0280490e78e755610bec09e
Jasper Froeling <jasper.froeling@gmail.com>
2017-03-11 23:31:42 +0100

    Scrape Jumbo prices

--- product/productservice.py
+++ product/productservice.py
@@ -1,5 +1,6 @@
 from questionmark import api
-from .models import Product, Category
+from .models import Product, Category, Score
+from questionmark.jumbo_scraper import JumboScraper
 from .questionmarkmapper import QuestionmarkMapper
 
 
@@ -7,16 +8,22 @@ class ProductService:
 
     @staticmethod
     def get_all_products():
-        ProductService.update_products_from_questionmarkapi("tomatenpuree")
+        ProductService.update_products_from_questionmarkapi("pindakaas")
 
         return Product.objects.all()
 
     @staticmethod
     def update_products_from_questionmarkapi(search_name):
         products_dict = api.search_product(search_name)
+        name_price_list = JumboScraper().get_search_result(search_name)
         products = []
         for product_dict in products_dict["products"]:
             product, created = Product.objects.get_or_create(name=product_dict["name"])
             product = QuestionmarkMapper().map_to_product(product, product_dict)
+            for name_price in name_price_list:
+                name = "Jumbo " + product.name.replace("0 g", "0g")
+                if name in name_price:
+                    product.price = int(name_price[name])
+                    product.save()
             products.append(product)
         return products
