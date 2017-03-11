8045bfe46480025a3c23d56ed84dc5b0fe7889ac
milanvanhoek <milanvanhoek@gmail.com>
2017-03-11 17:03:03 +0100

    Small  changes to algorithms.py

--- product/algorithms.py
+++ product/algorithms.py
@@ -3,58 +3,29 @@ from product.models import Product
 
 class ProductChooseAlgorithm:
 
-	@staticmethod
-	def return_product():
-		return ProductChooseAlgorithm.cheapest_product()
-	
-	def 
-		
-	@staticmethod
-	def cheapest_product():	
-		minPrice = 99999999999
-		if Product.objects:
-			productToReturn = Product.objects.all()[0]
-			for product in Product.objects.all():
-				if product.price.cent < minPrice:
-					minPrice = product.price.cent
-					productToReturn = product
-		
-			return productToReturn.name
-			
-		return None
-		
-	#returns the weighted sum over the user weights and the product scores	
-	@staticmethod
-	def objective_function(product, userweights):
-		counter = 0
-		sum = 0
-		while ( counter < len(product.Scores) ):
-			sum += product.Scores[counter] * userweights[counter]
-			counter++
-		
-		return sum
-		
-	@staticmethod
-	def maximizeProductScores(userweights):
-		maxScore = -99999999999
-		if Product.objects:
-			productToReturn = Product.objects.all()[0]
-			for product in Product.objects.all():
-				result = objective_function(product,userweights)
-				if result > maxScore:
-					maxScore = result
-					productToReturn = product
-		
-			return productToReturn, result)
-			
-		return None
-	
-		
-	
-	
-		
-		
-	
+    
+    #returns the weighted sum over the user weights and the product scores    
+    @staticmethod
+    def objective_function(product, userweights):
+        counter = 0
+        sum = 0
+        while ( counter < len(product.Scores) ):
+            sum += product.Scores[counter] * userweights[counter]
+            counter+=1        
+        return sum
+        
+    @staticmethod
+    def maximize_product_scores(userweights):
+        maxScore = -99999999999
+        if Product.objects.all():
+            productToReturn = Product.objects.all()[0]
+            for product in Product.objects.all():
+                result = ProductChooseAlgorithm.objective_function(product,userweights)
+                if result > maxScore:
+                    maxScore = result
+                    productToReturn = product
+            return (productToReturn, result)        
+        return None
 
     @staticmethod
     def return_product():
@@ -63,7 +34,7 @@ class ProductChooseAlgorithm:
     @staticmethod
     def cheapest_product():
         minPrice = 99999999999
-        if Product.objects:
+        if Product.objects.all():
             productToReturn = Product.objects.all()[0]
             for product in Product.objects.all():
                 price = product.price or 0
@@ -72,3 +43,5 @@ class ProductChooseAlgorithm:
                     productToReturn = product
             return productToReturn.name
         return None
+
+        
