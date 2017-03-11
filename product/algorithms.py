15847309b6c53d7311fa3d5ea0a352c71ca98f0b
Mathijs <Mathijs>
2017-03-11 15:15:38 +0100

    fixed indentations and so on

--- product/algorithms.py
+++ product/algorithms.py
@@ -1,24 +1,21 @@
-from product.models import Price, Product
+from product.models import Product
+
 
 class ProductChooseAlgorithm:
-	@staticmethod
-	def return_product():
-		return ProductChooseAlgorithm.cheapest_product()
-		
-	@staticmethod
-	def cheapest_product():	
-		minPrice = 99999999999
-		if Product.objects:
-			productToReturn = Product.objects.all()[0]
-			for product in Product.objects.all():
-				if product.price.cent < minPrice:
-					minPrice = product.price.cent
-					productToReturn = product
-		
-			return productToReturn.name
-			
-		return None
-			
-		
+    @staticmethod
+    def return_product():
+        return ProductChooseAlgorithm.cheapest_product()
 
-	
+    @staticmethod
+    def cheapest_product():
+        minPrice = 99999999999
+        if Product.objects:
+            productToReturn = Product.objects.all()[0]
+            for product in Product.objects.all():
+                if product.price.cent < minPrice:
+                    minPrice = product.price.cent
+                    productToReturn = product
+
+            return productToReturn.name
+
+        return None
