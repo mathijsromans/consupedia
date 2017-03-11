0ca681ec906dc8c44e5241c2182c37ceb2d21d42
Tijmen <theplunder@gmail.com>
2017-03-11 17:22:23 +0100

    added validation to price

--- product/forms.py
+++ product/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_price(value):
+    if value < 0:
+        raise ValidationError("Value cannot be below 0")
 
 
 class ProductForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
-    price = forms.IntegerField(label='Price (cents)')
+    price = forms.IntegerField(label='Price (cents)',validators=[validate_price])
