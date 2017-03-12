ad5b7326a980f09aa72666daaffa5e9979d5d4a7
Mathijs Romans <github@romansland.nl>
2017-03-12 14:49:12 +0100

    parsing of ingredient quantities

--- product/productservice.py
+++ product/productservice.py
@@ -27,13 +27,26 @@ class ProductService:
             product = qm_mapper.map_to_product(product, product_dict)
             for jumbo_result in jumbo_results:
                 name = "Jumbo " + product.name.replace("0 g", "0g")
-                print(name + "   ----   " + jumbo_result['name'])
+                # print(name + "   ----   " + jumbo_result['name'])
                 if jumbo_result['name'] == name:
                     jumbo_mapper.map_to_product(product, jumbo_result)
             products.append(product)
         return products
 
 class RecipeService():
+
+    @staticmethod
+    def get_quantity_and_unit( quantity, unit_text ):
+        if unit_text == '-' or unit_text == 'blaadjes':
+            return quantity, Ingredient.NO_UNIT
+        if unit_text == 'g':
+            return quantity, Ingredient.GRAM
+        if unit_text == 'kg':
+            return 1000*quantity, Ingredient.GRAM
+        if unit_text == 'el':
+            return quantity, Ingredient.EL
+        print('UNKNOWN UNIT : ' + unit_text)
+        return quantity, Ingredient.NO_UNIT
 
     @staticmethod
     def create_recipe( name,
@@ -44,7 +57,7 @@ class RecipeService():
                        preparation,
                        ingredient_input ):
         test_ingredients = [
-            [4, 'uien'],
+            [4, '-', 'uien'],
             [500, 'g', 'preien'],
             [40, 'g', 'boter'],
             [2, 'el', 'olijfolie'],
@@ -56,7 +69,7 @@ class RecipeService():
 
         if not ingredient_input:
             ingredient_input = test_ingredients
-        print('create recipe: ' + str(ingredient_input))
+        print('Creating recipe with ingredients: ' + str(ingredient_input))
         new_recipe = Recipe.objects.create(name = name,
                                            author_if_user=author_if_user,
                                            source_if_not_user = source_if_not_user,
@@ -73,5 +86,7 @@ class RecipeService():
             for p in products:
                 cat_dict[p.category] += 1
             max_cat = max(cat_dict.items(), key=(lambda a: a[1]))
-            print(max_cat[0])
-            Ingredient.objects.create(quantity=ing[0], unit=Ingredient.NO_UNIT, category=max_cat[0], recipe = new_recipe)
+            quantity, unit = RecipeService.get_quantity_and_unit( ing[0], ing[1])
+            print('Using ingredient ' + str(quantity) + ' ' + str(unit) + ' ' + str(max_cat[0]))
+            Ingredient.objects.create(quantity=quantity, unit=unit, category=max_cat[0], recipe = new_recipe)
+        print('Done creating recipe')
