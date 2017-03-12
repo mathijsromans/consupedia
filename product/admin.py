749cebd2fe108723499537f4cb4d98311491a6d5
Bart Römgens <bart.romgens@gmail.com>
2017-03-12 11:22:40 +0100

    show product properties in admin list view

--- product/admin.py
+++ product/admin.py
@@ -3,7 +3,13 @@ from product.models import Rating
 from product.models import Product
 from django.contrib import admin
 
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ('name', 'price', 'amount_in_gram', 'category', 'product_score')
+
+
 admin.site.register(UserPreferences)
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Rating)
