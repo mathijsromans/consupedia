bc9c6571e8be808fe79d4fcc38c647eb74bbc881
Mathijs Romans <github@romansland.nl>
2017-12-16 13:45:24 +0100

    added amount test

--- product/tests.py
+++ product/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from api import allerhande_scraper
 
 from .productservice import RecipeService
+from .amount import ProductAmount
 
 
 class TestCreateRecipe(TestCase):
@@ -21,3 +22,19 @@ class TestCreateRecipe(TestCase):
 
         )
         print(recipe_new)
+
+
+class TestAmount(TestCase):
+    recipe_id = 'R-R399568'
+
+    def test_translate(self):
+        self.assertEqual(ProductAmount.get_quantity_and_unit(200, 'g'), (200, 'g'))
+        self.assertEqual(ProductAmount.get_quantity_and_unit(200, 'gr'), (200, 'g'))
+        self.assertEqual(ProductAmount.get_quantity_and_unit(200, 'gram'), (200, 'g'))
+        self.assertEqual(ProductAmount.get_quantity_and_unit(0.2, 'kg'), (200, 'g'))
+
+        self.assertEqual(ProductAmount.get_quantity_and_unit(200, 'ml'), (200, 'ml'))
+        self.assertEqual(ProductAmount.get_quantity_and_unit(20, 'cl'), (200, 'ml'))
+        self.assertEqual(ProductAmount.get_quantity_and_unit(0.2, 'l'), (200, 'ml'))
+        self.assertEqual(ProductAmount.get_quantity_and_unit(0.2, 'L'), (200, 'ml'))
+        self.assertEqual(ProductAmount.get_quantity_and_unit(0.2, 'liter'), (200, 'ml'))
