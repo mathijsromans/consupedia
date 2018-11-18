8da4f51f7dc9595ef742d19338b08fbdea84c1e1
Mathijs Romans <github@romansland.nl>
2018-11-18 23:46:36 +0100

    Order recipes

--- product/views.py
+++ product/views.py
@@ -75,7 +75,7 @@ class RecipesView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        recipes = Recipe.objects.all()
+        recipes = Recipe.objects.all().order_by('name')
         context['recipes'] = recipes
         return context
 
