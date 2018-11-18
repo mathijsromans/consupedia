c27dc787d7bfe4147b542a8d79c897064283ca49
Bart Römgens <bart.romgens@gmail.com>
2018-11-18 15:21:22 +0100

    Fix no default ScoreCreator on new database
    Add migration to create a default scorer

---
+++ product/migrations/0022_add_default_scorecreator.py
@@ -0,0 +1,36 @@
+import uuid
+
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+
+def create_admin_user(apps, schema_editor):
+    User = apps.get_registered_model('auth', 'User')
+    user_default = User(
+        username='default',
+        email='default@default.com',
+        password=make_password(uuid.uuid4().hex),
+        is_superuser=False,
+        is_staff=False
+    )
+    user_default.save()
+
+
+def create_types(apps, schema_editor):
+    db_alias = schema_editor.connection.alias
+    Type = apps.get_model("product", "ScoreCreator")
+    User = apps.get_registered_model('auth', 'User')
+    user_default = User.objects.get(email='default@default.com')
+    Type.objects.using(db_alias).create(name="default", user=user_default)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+         ('product', '0021_auto_20181117_2332'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_admin_user),
+        migrations.RunPython(create_types),
+    ]
