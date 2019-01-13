9cf80f40492d63da23bf064a79f2021f625ae3cd
Sjoerd Kuperus <sjoerdkuperus@gmail.com>
2019-01-13 19:14:05 +0100

    Question 2

--- website/static/js/questionnaire.js
+++ website/static/js/questionnaire.js
@@ -1,8 +1,12 @@
-$("#priceNotImportant").click(function(e) { saveUserPreference(e, "price", 10); });
+$("#priceNotImportant").click(function(e) { saveUserPreference(e, "price", 1); });
 $("#priceMediumImportant").click(function(e) { saveUserPreference(e,"price", 50); });
 $("#priceVeryImportant").click(function(e) { saveUserPreference(e,"price", 100); });
 
-function saveUserPreference(event, preference, newWeight) {
+$("#noEatMeat").click(function(e) { saveUserPreference(e,"animals", 100, function(){ saveUserPreference(e,"animal_harm", 100);}); });
+$("#sometimesEatMeat").click(function(e) { saveUserPreference(e,"animals", 50, function(){ saveUserPreference(e,"animal_harm", 50);}); });
+$("#alwaysEatMeat").click(function(e) { saveUserPreference(e,"animals", 1, function(){ saveUserPreference(e,"animal_harm", 1);}); });
+
+function saveUserPreference(event, preference, newWeight, callback) {
 	$(event.target).parent().find(".questionnaire-answer_selected").removeClass("questionnaire-answer_selected");
 	$(event.target).addClass("questionnaire-answer_selected");
 	$.post(
@@ -14,6 +18,10 @@ function saveUserPreference(event, preference, newWeight) {
 	    function(data) {
 			if(data.status == 'success')
 			{
+				if(callback != null && typeof callback === "function")
+				{
+					callback();
+				}
 				console.log("weight changed.")
 			}
 	    },
